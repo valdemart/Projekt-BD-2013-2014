0e762c207958a92db9cb4a4fb2d7e6c1b5fa775f
monika17 <monikakaczmarek177@gmail.com>
2014-01-05 20:29:29 +0100

    proba1
    uzupelnienie1.txt to poki co to co uzupelnilam bo nie jestem pewna jak
    wyeksportowac baze danych zebys mial to co ja wpisalam, wiec na wszelki
    wypadek w txt zebys mogl sobie szybko skopiowac.

---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pobieracz_Click(object sender, EventArgs e)
+        {
+            //jodb baza = new jodb("monika", "12345", ".", "wesela");
+            jodb baza_win = new jodb("(local)", "wesela");
+            dataGridView1.DataSource = baza_win.pobierz_dane("select * from osoby");
+        }
+
+    }
+}
